df294d695db65e1d117453e941959b9611e76f76
ndgspn <nandangsopyan08@gmail.com>
2019-05-04 20:15:02 +0700

    soal_5.rb

---
+++ soal_5.rb
@@ -0,0 +1,32 @@
+class RandomString
+
+  def generate(n)
+    ran_arr = []
+    while ran_arr.length < n do
+      key = random(32, alphabet + numbers)
+      ran_arr << key
+      ran_arr.uniq
+    end
+    ran_arr
+  end
+  
+  private
+
+  def random(length, charset)
+    Array.new(length) { charset.sample }.flatten.join
+  end
+
+  def alphabet
+    @alphabet ||= Array('A'..'Z') + Array('a'..'z')
+  end
+
+  def numbers
+    @numbers ||= Array(0..9)
+  end
+end
+
+r = RandomString.new
+puts r.generate(3)
+puts "======"
+puts r.generate(5)
+
