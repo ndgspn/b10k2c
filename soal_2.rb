78194c3e71ca18173ae4d207fbca14510c358cb3
ndgspn <nandangsopyan08@gmail.com>
2019-05-04 16:05:52 +0700

    added soal_2.rb

---
+++ soal_2.rb
@@ -0,0 +1,42 @@
+class Register
+  attr_accessor :username, :password
+
+  def initialize(username, password)
+    @username   = username
+    @password   = password
+  end
+
+  def call
+    register_user
+  end
+
+  def register_user
+   if validate_user && validate_password 
+     true
+   else
+     false
+   end
+  end
+
+  def validate_user
+    if username == nil || username.length < 8
+      false
+    else
+      true
+    end
+  end
+
+  def validate_password
+    return true if password.length >= 8
+  end
+end
+
+reg1 = Register.new('john', '12345678').call
+reg2 = Register.new('naruto123', '123').call
+reg3 = Register.new('naruto123', '12345678').call
+reg4 = Register.new('', 'pass').call
+
+puts reg1
+puts reg2
+puts reg3
+puts reg4
