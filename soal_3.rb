d18c653c9edfc2d627904eac3059b4fd34600d41
ndgspn <nandangsopyan08@gmail.com>
2019-05-09 17:07:36 +0700

    added validation number and error rescue

--- soal_3.rb
+++ soal_3.rb
@@ -1,13 +1,29 @@
 def square(n)
-  1.upto(n) do | row |
-    if row % 3 != 0
-      puts "#{'*'}#{'#' * (n - 2)}#{'*'}"
+  begin
+    if n.odd?
+      1.upto(n) do | row |
+        if row % 3 != 0
+          puts "#{'*'} #{'= ' * (n - 2)}#{'*'}"
+        else
+          puts "#{'* ' * n}"
+        end
+      end
     else
-      puts "#{'*' * n}"
+      puts 'Must odd number!'
     end
+  rescue
+    puts 'Must integer number!'
   end
 end
 
 square(5)
 puts "---------"
 square(7)
+puts "---------"
+square(8)
+puts "---------"
+square(8.5)
+puts "---------"
+square('blabla')
+puts "---------"
+square("8")
